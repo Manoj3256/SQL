8624e997eedbe9a40d164c25469ec9bef71ec19a
Manoj S <3256mmanoj@gmail.com>
2026-03-16 14:12:04 +0530

    Time: 404 ms (13.66%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+select visited_on,
+    sum(amount) over(
+        order by visited_on
+        rows between 6 preceding and current row) as amount,
+        round(avg(amount) over(
+        order by visited_on
+        rows between 6 preceding and current row),2) as average_amount
+from (select visited_on, sum(amount)as amount
+    from Customer
+    group by visited_on) day_wise
+order by visited_on
+limit 500 offset 6
